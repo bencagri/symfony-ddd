77fe9065b0c9d34774b74fb049e820d07c27e59f
bencagri <selam@bencagri.com>
2018-03-11 01:45:07 +0300

    add nelmio doc bundle

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     FOS\OAuthServerBundle\FOSOAuthServerBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
