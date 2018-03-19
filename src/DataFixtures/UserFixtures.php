2666ee4de3550c714ba9a6ba1978cda4eeeca870
cagri <cagri@pionix.ro>
2018-03-19 14:51:46 +0200

    add user fixture

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Aurora\Domain\User\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('Aurora');
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
